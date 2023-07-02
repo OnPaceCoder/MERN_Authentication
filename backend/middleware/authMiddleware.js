182bf0b35559a10c611aac9d9d2455754d41af2e
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-02 12:13:23 +0530

    User updated profile feature implemented

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -22,7 +22,7 @@ const protect = asyncHandler(async (req, res, next) => {
 
     } else {
         res.status(401);
-        throw new Error("Not authrized, no token");
+        throw new Error("Not authorized, no token");
     }
 })
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authUser, registerUser, logout, getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router()
 
 
 router.post('/', registerUser)
 router.post('/auth', authUser)
 router.post('/logout', logout)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router;

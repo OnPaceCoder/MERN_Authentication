6fa871eb1b62e853324a17c154c1e432d214bc33
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-02 11:43:48 +0530

    Authentication middleware created

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDb from './config/db.js';
 dotenv.config();
 const port = process.env.PORT || 4000;
 connectDb()
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(cookieParser())
 app.use('/api/users', userRoutes)
 
 app.use(notFound);
